26a23447680031fdbcd7ad0a3e363c9c288334cb
StevenZ315 <60725360+StevenZ315@users.noreply.github.com>
2020-06-21 22:02:19 -0700

    Update test.py

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ print()
 
 
 
-def linear_result(model):
+def regressor_evalution(model):
     X, y = make_regression(n_samples=200, n_features=1, noise=20, random_state=42)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -21,7 +21,7 @@ def linear_result(model):
     plt.plot(X_test, y_pred, color='black', linewidth=2, label='Prediction')
     plt.show()
 
-def logistic_result(model):
+def classifier_evaluation(model):
     # X, y = make_classification(n_samples=200, n_features=4, n_classes=2, random_state=4)
     X, y = load_breast_cancer(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -32,5 +32,5 @@ def logistic_result(model):
     print("Final Test Score: %.4f" % test_score)
 
 
-logistic_result(LogisticRegression())
+classifier_evaluation(LogisticRegression())
 
